b3a5dc05c25960dcb9a952fa9b8c9e99b42e7827
Satria-min <askleye@gmail.com>
2026-09-16 11:32:37 +0700

    Add files via upload

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ysnmheoreyvipzirabeh.supabase.co'
-const supabaseAnonKey = 'sb_publishable_DHU-AGBAgQ5G0CRwa4nczw_N0dh-3Ii'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlzbm1oZW9yZXl2bHB6aXJhYmVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzODU3NTAsImV4cCI6MjEwNDk2MTc1MH0.KQ0fhBcn-VgYi_KltuBsaEiHPaHSGuvfIL2xvmCirlM'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
